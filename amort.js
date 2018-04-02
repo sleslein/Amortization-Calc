a4417e746b850d01cad759b4ac0bb930645d0783
sleslein <shea.leslein@gmail.com>
2018-04-01 21:24:38 -0500

    Added parts for creating amortization schedule.

--- amort.js
+++ amort.js
@@ -4,10 +4,6 @@ const calcPeriodRate = function(r) {
     return r/12;
 };
 
-// function calcPeriodRate(r) {
-//     return r/12;
-// };
-
 function calcMontlyPayment(principal, rate, payments) {
     const convertedRate = rate/100;
     const periodicRate = calcPeriodRate(convertedRate);
@@ -18,7 +14,31 @@ function calcTotalCost(monthlyPmt, totalPayments) {
     return monthlyPmt * totalPayments;
 }
 
-function calcTotalInterest(totalCost, principal)
-{
+function calcTotalInterest(totalCost, principal) {
     return totalCost - principal;
+}
+
+function calcMonthyInfo (payment, principle, interestPercent) {
+    var i = interestPercent * payment; // total toward interest
+    var p = payment - i; //total toward principle
+
+    return {
+        balance: principle - p,
+        interest: i,
+        principal: p
+    };
+}
+
+function calcAmortizationSchedule(principle, monthlyInterestPercent, montlyPayment) {
+    var items = [];
+    let p = principle;
+
+    while (p > 0) {
+        const item = calcMonthyInfo(montlyPayment, p, monthlyInterestPercent);
+        p = p - item.principal;
+
+        items.push(item);
+
+    }
+    return items;
 }
--- app.js
+++ app.js
@@ -6,14 +6,19 @@ function getMonthlyPayment(){
     var totalPayments = document.getElementById("txtTotalMonthlyPayments").value;
 
     // Caclulate data
+    var monthlyInterestRate = calcPeriodRate(interestRate);
     var monthlyPayment = calcMontlyPayment(loanAmount, interestRate, totalPayments);
     var totalCost = calcTotalCost(monthlyPayment, totalPayments);
     var totalInterest = calcTotalInterest(totalCost, loanAmount);
+    var scheduleItems = calcAmortizationSchedule(loanAmount, monthlyInterestRate, monthlyPayment);
 
     // Assign values to view
     document.getElementById("txtMonthlyPayment").value = monthlyPayment;
     document.getElementById("txtTotalCost").value = totalCost;
     document.getElementById("txtTotalInterest").value = totalInterest;
+
+    // Add grid items for each schedule Item
+    // https://www.w3schools.com/js/js_htmldom_nodes.asps
 }
 
 document.getElementById("btnCalculate").addEventListener("click", getMonthlyPayment);
